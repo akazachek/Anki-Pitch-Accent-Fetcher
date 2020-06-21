2149ddc34ba4a991024966dde9caa4bf0d2baf12
akazachek <alexdkazachek@gmail.com>
2020-06-20 20:42:08 -0400

    Navigates Anki collection, creating a list of relevant card IDs and the corresponding words.

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-import sqlite3
+from anki import Collection
 from aqt import mw
-from aqt.utils import showInfo
-from aqt.qt import *
+import json
+
+col = Collection("collectionCopy.anki2")
+deckName = "Core 2k/6k Optimized Japanese Vocabulary"
+
+decks = col.db.scalar("SELECT decks FROM col")
+cards = col.db.all("SELECT nid,did FROM cards")
+notes = col.db.all("SELECT id,sfld FROM notes")
+fetchedNoteIDs = []
+fetchedCards = []
+decksJSON = json.loads(decks)
+deckID = -1
+
+# multidim dict navigation
+for deck in decksJSON:
+    deckInfo = decksJSON[deck]
+    if deckInfo["name"] == deckName:
+        deckID = deckInfo["id"]      
+        
+# creates list of relevant note IDs
+for i in range(len(cards)):
+        if cards[i][1] == deckID:
+            fetchedNoteIDs.append(cards[i][0])
+
+# creates list of cards corresponding to each note ID, as well as their search field
+# in this case, search field is the word
+for ID in fetchedNoteIDs:
+    cardID = col.findCards("nid:" + str(ID))[0]
+    noteSFLD = -1
+    for i in range(len(notes)):
+        if notes[i][0] == ID:
+            noteSFLD = notes[i][1]
+    fetchedCards.append([cardID, noteSFLD])
+
+print(fetchedCards)
 
